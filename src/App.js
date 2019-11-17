8d4e57458eaa39c36d67bddd3adb544e79f6033b
Brian <brian19931009@gmail.com>
2019-11-17 22:03:55 +0800

    App structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <TodoInput />
-          <TodoList />
+class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 1, title: "make breakfast" }
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = e => {};
+
+  handleSubmit = e => {};
+
+  clearList = () => {};
+
+  handleDelete = id => {};
+
+  handleEdit = id => {};
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-5"></div>
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
