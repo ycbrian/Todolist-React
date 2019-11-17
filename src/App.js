65df33b333fab097f0d1e30d89167ff7df1e596a
Brian <brian19931009@gmail.com>
2019-11-17 22:04:17 +0800

    App structure complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import { isParenthesizedExpression } from "@babel/types";
 
 class App extends Component {
   state = {
@@ -32,8 +33,18 @@ class App extends Component {
           <div className="row">
             <div className="col-10 mx-auto col-md-8 mt-5"></div>
             <h3 className="text-capitalize text-center">todo input</h3>
-            <TodoInput />
-            <TodoList />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
