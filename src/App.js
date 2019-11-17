401dc9efdccf3102570d0688cf52f99824ae4e7e
Brian <brian19931009@gmail.com>
2019-11-17 22:19:28 +0800

    Input Component complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-import { isParenthesizedExpression } from "@babel/types";
 
 class App extends Component {
   state = {
@@ -31,20 +30,21 @@ class App extends Component {
       <div>
         <div className="container">
           <div className="row">
-            <div className="col-10 mx-auto col-md-8 mt-5"></div>
-            <h3 className="text-capitalize text-center">todo input</h3>
-            <TodoInput
-              item={this.state.item}
-              handleChange={this.handleChange}
-              handleSubmit={this.handleSubmit}
-              editItem={this.state.editItem}
-            />
-            <TodoList
-              items={this.state.items}
-              clearList={this.clearList}
-              handleDelete={this.handleDelete}
-              handleEdit={this.handleEdit}
-            />
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">todo input</h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
           </div>
         </div>
       </div>
